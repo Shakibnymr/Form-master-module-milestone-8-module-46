9a3f58636bcc395cf2f8b7b89c6db07b12bf4f50
shakibnymr <shakibnymr11@gmail.com>
2023-09-22 00:54:07 +0600

    ..

--- src/__form__.js
+++ src/__form__.js
@@ -3,6 +3,6 @@
  * 2. Form access e.target.[name of the input field].value
  * 3.controlled element: use individual field state for each input field
  * 4.controlled element: one object in a state holding all the input fields
- *  5.uncontrolled element:
+ *  5.uncontrolled element:useRef to create a reference to the element and access value by using like: nameRef.current.value
  * (useRef)
  * */ 
